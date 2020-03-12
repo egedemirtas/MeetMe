bdf4fcf51918fa76815c52b5f4ce7b2fcf6d35ac
egedemirtas <egedemirtas@sabanciuniv.edu>
2020-03-12 13:29:28 +0300

    Registration is modified

--- MeetMe/accounts/views.py
+++ MeetMe/accounts/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
-
-# Create your views here.
+from .forms import UserRegisterForm
+from django.forms import inlineformset_factory
 
 def register(request):
+    """
     if request.method == 'POST':
         first_name = request.POST['first_name']
         last_name = request.POST['last_name']
@@ -29,6 +30,20 @@ def register(request):
             return redirect('register')
     else:
         return render(request, 'accounts/register.html')
+    """
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account Created for {username}')
+            return redirect('register')
+        else: 
+            messages.warning(request, f'Account Name is taken')
+    else:
+        form = UserRegisterForm();
+    return render(request, 'accounts/register.html', {'form':form})
+
 def login(request):
      if request.method=='POST':
          username=request.POST['username']
