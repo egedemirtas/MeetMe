90504e3421144e34f6a062ba4c5f8f62ce4f2a57
K.Eren Unal <keren@sabanciuniv.edu>
2020-04-09 14:17:43 +0300

    Logout page creation and linked

--- MeetMe/accounts/urls.py
+++ MeetMe/accounts/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
 from . import views
-#from django.contrib.auth import views as auth_views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('register/', views.register, name='register'),
-    path('login/', views.login, name='login')
+    path('login/', views.login, name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name= 'accounts/logout.html'), name= 'logout')
     #auth_views.login.as_view(template_name= 'users/login.html')path('logout/', auth_views.LogoutView.as_view(template_name= 'users/logout.html'), name= 'logout')
 ]
