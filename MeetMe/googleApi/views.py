ad4f07e7551b4e39f0f43f82f63ff13f1c137d38
efehanguner <efehanguner@sabanciuniv.edu>
2020-04-09 12:49:38 +0300

    Google Api DB implementation

--- MeetMe/googleApi/views.py
+++ MeetMe/googleApi/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from django.contrib import messages
+from eventCalendar.models import Events
+
 
 def gCalendar(request):
     flow = InstalledAppFlow.from_client_secrets_file(
@@ -10,12 +12,28 @@ def gCalendar(request):
     auth_uri = flow.authorization_url()
 
     credentials=flow.run_local_server()
-    print(credentials)                     ##
+    #print(credentials)                     ##
     service=build("calendar","v3",credentials=credentials)
     calendars=service.calendarList().list().execute()
-    #calendar_id=calendars['items'][0]['id']  ##primary calendar also can be called via primary keyword
-    events=service.events().list(calendarId='primary').execute()
-    print(events)  ##
-    events=str(events)
-    messages.info(request,events)
+    events=service.events().list(calendarId='primary').execute() ##get all events
+    #print(events)  ##
+    #print(type(events))
+    #events=str(events)
+    #messages.info(request,events) 
+    # #summary  title  
+    for item in events['items']: 
+        title=item['summary']
+        start=item['start'].get('dateTime','NA')
+        end=item['end'].get('dateTime','NA')
+        if(start=='NA'):   ##For unspecified hour/minutes
+            start=item['start']['date']
+        if(end=='NA'):
+            end=item['end']['date']
+        #print(end.keys())
+        print('{}, {}, {}'.format(title,start,end))
+        event = Events(name=str(title), start=start, end=end)
+        if not(Events.objects.filter(name=title,start=start,end=end).exists()): ##if it is not in db
+            event.save()
+
+
     return render(request,'googleApi/a.html')
