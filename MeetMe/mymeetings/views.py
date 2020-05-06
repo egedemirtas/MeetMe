7d7629ac51097c03bdbb31d82d072453a92c5f05
efehanguner <efehanguner@sabanciuniv.edu>
2020-05-06 23:14:52 +0300

    Small change to db calculations *NOT TESTED

--- MeetMe/mymeetings/views.py
+++ MeetMe/mymeetings/views.py
@@ -11,17 +11,20 @@ def myMeetings(request):
     return render(request,'mymeetings/myMeetings.html',context)  ##testing
     #return render(request,'eventCalendar/addMeeting.html',context)
 def voting(request):
+    user=request.user
     meetingID=16 ###
     #meetingID=request.POST('meetingID_r')
     options=MeetingEvents.objects.filter(meetingID = meetingID)
+    parc=MeetingParticipation.objects.get(meetingID = meetingID ,partID=user.id)
     #print(options[0].voteNumber)
     ## it should wait for response from the front
     if request.method=='POST':
         MeetingEventID=request.POST['id']
-        result=MeetingEvents.object.get(MeetingEventID = 1)
+        result=MeetingEvents.objects.get(MeetingEventID = 1)
         result.voteNumber+=1
         result.save()
+        parc.is_voted=True
+        parc.save()
 
-    #return render(request,'mymeetings/voting.html')
     return render(request,'mymeetings/voting.html', {'options': options})
     
